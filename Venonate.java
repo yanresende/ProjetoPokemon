c0d5308aa890ba3c812ebf7df2ae040d37b26420
yanresende <saresende555@gmail.com>
2024-11-10 11:57:52 -0300

    venonate e venossauro ajustados

--- Venonate.java
+++ Venonate.java
@@ -3,66 +3,42 @@ public class Venonate extends Pokemon {
         this.setNome("Venonate");
         this.setVida(20);
         this.setNivel(1);
-        this.setTipo("Planta");
-        this.setTreinador("Treinador Desconhecido");
-        this.setVantagem("Agua-Pedra-Terra");
+        this.setTipo("Venenoso");
+        this.setVantagem("Planta-Fada");
     }
 
     public Venonate(int vida, int nivel, String treinador) {
-        this.setNome("Venonate");
+        this();
         this.setVida(vida);
         this.setNivel(nivel);
-        this.setTipo("Planta");
         this.setTreinador(treinador);
-        this.setVantagem("Agua-Pedra-Terra");
     }
 
     @Override
-    public int Ataque(int i, boolean v) {
-        if (i == 1) {
-            System.out.println("Venonate usou Fly");
-            if (v == true) {
-                i = 1 * 2;
-                System.out.println("- " + i + " de vida");
-            } else {
-                i = 1;
-                System.out.println("- " + i + " de vida");
-            }
-            return i;
-        } else if (i == 2) {
-            System.out.println("Venonate usou Bite");
-            if (v == true) {
-                i = 3 * 2;
-                System.out.println("- " + i + " de vida");
-            } else {
-                i = 3;
-                System.out.println("- " + i + " de vida");
-            }
-            return i;
-        } else if (i == 3) {
-            System.out.println("Venonate usou Fire Ball");
-            if (v == true) {
-                i = 5 * 2;
-                System.out.println("- " + i + " de vida");
-            } else {
-                i = 5;
-                System.out.println("- " + i + " de vida");
-            }
-            return i;
-        } else if (i == 4) {
-            System.out.println("Venonate usou Flame Tower");
-            if (v == true) {
-                i = 10 * 2;
-                System.out.println("- " + i + " de vida");
-            } else {
-                i = 10;
-                System.out.println("- " + i + " de vida");
-            }
-            return i;
-        } else if (i < 1 && i > 4) {
-            System.err.println("Nada acontece");
+    public int ataque(int tipoAtaque, boolean temVantagem) {
+        int dano;
+        switch (tipoAtaque) {
+            case 1:
+                System.out.println("Venonate usou Fly");
+                dano = temVantagem ? 2 : 1;
+                break;
+            case 2:
+                System.out.println("Venonate usou Bite");
+                dano = temVantagem ? 6 : 3;
+                break;
+            case 3:
+                System.out.println("Venonate usou Fire Ball");
+                dano = temVantagem ? 10 : 5;
+                break;
+            case 4:
+                System.out.println("Venonate usou Flame Tower");
+                dano = temVantagem ? 20 : 10;
+                break;
+            default:
+                throw new IllegalArgumentException("Ataque inválido!");
         }
-        return i;
-
+        System.out.println("- " + dano + " de vida");
+        return dano;
     }
+
 }
